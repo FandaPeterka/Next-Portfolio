71ca92ef28f036039a7a1c2e1c6e9cb7d9348a63
František Peterka <frantisekpeterka@Frantisek--MacBook-Pro.local>
2025-07-10 12:24:54 +0200

    patch

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.frantisekpeterka.com',
-  generateRobotsTxt: true,      // vygeneruje i robots.txt
+  siteUrl: process.env.SITE_URL || 'https://www.frantisekpeterka.com',
+  generateRobotsTxt: true,  // kromě sitemap.xml se vygeneruje i robots.txt
   sitemapSize: 7000,
-};
+}
